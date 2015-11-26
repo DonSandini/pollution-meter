4a47aad92dcaf730758fde3098cff75aae50aa17
Alexander Georgiev <donsandini@gmail.com>
2015-11-27 00:54:59 +0200

    ajax test

--- App.js
+++ App.js
@@ -2,10 +2,45 @@
  * Created by Sasho on 25/11/2015.
  */
 import React from 'react';
-class App extends React.Component {
+import ReactDOM from 'react-dom';
+
+var App = React.createClass({
+    getInitialState() {
+        return {
+            username: '',
+            lastGistUrl: ''
+        };
+    },
+
+    componentDidMount() {
+        var newName = 'John Smith',
+            xhr = new XMLHttpRequest();
+
+        xhr.open('POST',
+            encodeURI(this.props.source));
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onload = function() {
+            if (xhr.status === 200 && xhr.responseText !== newName) {
+                debugger;
+            }
+            else if (xhr.status !== 200) {
+                debugger;
+            }
+        };
+        xhr.send(encodeURI('name=' + newName));
+
+    },
+
     render() {
-        return <div>Hello</div>
+        return (
+            <div>
+                {this.state.username}'s last gist is
+                <a href={this.state.lastGistUrl}>here</a>.
+            </div>
+        );
     }
-}
+});
+
+ReactDOM.render(<App source="https://api.github.com/users/octocat/gists" />, document.getElementById('app'));
 
 export default App
